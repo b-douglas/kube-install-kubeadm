d7e9934ad8d96f6307e539e6800aaf554ceb12c3
Doug Robinson <33792770+b-douglas@users.noreply.github.com>
2020-03-04 08:40:48 -0500

    Update 3g-kubecluster-join.sh

--- 3g-kubecluster-join.sh
+++ 3g-kubecluster-join.sh
@@ -1,11 +1,12 @@
+#!/bin/bash
 
-
-
-#echo "$(hostname -i) 		kubework01" >> /etc/hosts
-
-
+set -u 
+set -e
 set -x
 
-kubeadm join kubecp-01:6443 --token e4bim8.luofsi7hhxry236c \
-    --discovery-token-ca-cert-hash sha256:c5b540db55174546d7858b169e46d9601a173cd5770340ee759a8369a24524e7
+CP=$1
+NODE=$2
+TOKEN=$3
+SHA=$5
 
+sudo kubeadm join ${CP}:6443  --node-name ${NODE} --token ${TOKEN} --discovery-token-ca-cert-hash ${SHA}
